3afd22676ce5e0f2ad599efdb8496d242b78617c
taiberium <2554842Karim>
2021-06-15 09:47:56 +0300

    Add new versions of dependencies

--- src/main/java/com/finview/back/config/SwaggerConfig.java
+++ src/main/java/com/finview/back/config/SwaggerConfig.java
@@ -7,15 +7,13 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        //http://localhost:8080/swagger-ui.html
+        //http://http://localhost:8080/swagger-ui/
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
