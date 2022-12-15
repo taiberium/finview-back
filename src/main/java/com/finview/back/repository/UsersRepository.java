1e91da3099e0a0a67b5dda18c8e19ea77a4b5c4d
taiberium <akhmetovrustam1990@gmail.com>
2022-12-15 11:38:00 +0400

    Move to SpringBoot3 and remove old code or unused code

--- src/main/java/com/finview/back/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finview.back.repository;
-
-import com.finview.back.model.user.User;
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-
-public interface UsersRepository extends KeyValueRepository<User, String> {
-}
